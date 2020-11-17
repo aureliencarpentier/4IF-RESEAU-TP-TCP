1bbedea470daed6aba2a1380f0b5d70292b735f4
unknown <mariolopesgancarski@gmail.com>
2020-11-17 10:46:51 +0100

    while manquant

--- src/stream/client/Client.java
+++ src/stream/client/Client.java
@@ -67,11 +67,13 @@ public class Client {
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
+				while (true) {
 	        	try {
 					System.out.println("echo: " + socIn.readLine());
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+				}
 				}
 			}
         });
